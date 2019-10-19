62ee9cee6a408d12d384eaec29581c91bd2b96bc
TechTarun <40317745+TechTarun@users.noreply.github.com>
2019-10-19 11:08:52 +0530

    Add files via upload

---
+++ homeapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from pymongo import MongoClient
+
+def index(request):
+    string = "There is drainage problem in my locality. I have complained but nothing happened."
+    client = MongoClient("mongodb://tarun:tarun@hacknovate-shard-00-00-stlyh.mongodb.net:27017,hacknovate-shard-00-01-stlyh.mongodb.net:27017,hacknovate-shard-00-02-stlyh.mongodb.net:27017/test?ssl=true&replicaSet=Hacknovate-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.get_database("site_data")
+    records = db.sample
+    record_list = list(records.find())
+    return render(request, 'index.html', {'text':string, 'list':record_list})
+
+
+def home(request):
